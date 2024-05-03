64b1b97e4d2668de96389b8b02db052c20be0b1e
Markus Nevil <37672597+markus-nevil@users.noreply.github.com>
2024-05-03 15:58:05 -0400

    Update transgenic_longread_map.sh
    added parameters and fastq directory/file handling

--- transgenic_longread_map.sh
+++ transgenic_longread_map.sh
@@ -5,51 +5,128 @@
 #SBATCH --time=5:00:00
 #SBATCH --mem=16g
 
-#########################################################################################################################################
-#                                                                                                                                       #
-#   Run the script with:                                                                                                                #
-#   sbatch --time=5:00:00 --mem=16g --ntasks=2 --wrap="sh ./scripts/transgenic_longread_map.sh <ref.fasta> <query.fasta> [min_length]"  #
-#                                                                                                                                       #
-#########################################################################################################################################
+#################################################################################################################################################################################################################
+#                                                                                                                                                                                                               #
+#   Run the script with:                                                                                                                                                                                        #
+#   sbatch --time=5:00:00 --mem=16g --ntasks=2 --wrap="sh ./scripts/transgenic_longread_map.sh --ref <ref.fasta> --query <query.fasta> -- size [min_length] --fastqs <.FASTQ, .FASTQ.GZ, directories, or mix>"  #
+#                                                                                                                                                                                                               #
+#######################################################################################################################################################################1#########################################
+# Initialize our own variables
+ref_fasta=""
+query_fasta_file=""
+length=0
+found_fastq=""
+declare -a fastqs
 
-# Check if at least two FASTAs were provided
-if [[ -z "$2" ]]; then
-    echo "Usage: $0 <reference_fasta_file> <query_fasta_file> [length]"
-    exit 1
+# A function to display a usage message
+usage() { echo "Usage: $0 [--ref REF] [--query QUERY] [--size SIZE] [--fastqs FASTQS]" 1>&2; exit 1; }
+
+
+# Process the arguments
+while (( "$#" )); do
+  case "$1" in
+    --ref)
+      ref_fasta="$2"
+      shift 2
+      ;;
+    --query)
+      query_fasta_file="$2"
+      shift 2
+      ;;
+    --size)
+      length="$2"
+      shift 2
+      ;;
+    --fastqs)
+      while [[ "$2" != --* && -n "$2" ]]; do
+        fastqs+=("$2")
+        shift
+      done
+      shift
+      ;;
+    --) # end argument parsing
+      shift
+      break
+      ;;
+    -*|--*=) # unsupported flags
+      echo "Error: Unsupported flag $1" >&2
+      exit 1
+      ;;
+    *) # preserve positional arguments
+      PARAMS="$PARAMS $1"
+      shift
+      ;;
+  esac
+done
+
+# Process --fastqs
+for i in "${fastqs[@]}"; do
+    # process "$i"
+    if [[ -d $i ]]; then
+        # If directory, find all .fastq files recursively
+        fastq_files=$(find $i -type f \( -name "*.fastq" -o -name "*.fastq.gz" \) ! -name "concat.fastq")
+        #echo "Fastq files in directory $i:"
+        #echo "$fastq_files"
+    elif [[ -f $i ]]; then
+        # If file, check if it ends with .fastq and is not named concat.fastq
+        if [[ ($i == *.fastq || $i == *.fastq.gz) && $i != "concat.fastq" ]]; then
+            #echo "Fastq file: $i"
+            fastq_files=$i
+        fi
+    else
+        echo "$i is not a valid file or directory"
+    fi
+    found_fastq+=$fastq_files$'\n'
+done
+
+if ! [[ $length =~ ^[0-9]+$ ]] || [ $length -le 0 ]; then
+    echo "Warning: --size should be an integer greater than 0. Defaulting to 5000"
+    length=5000
 fi
 
-if [[ $1 != *.fa && $1 != *.fasta ]] || [[ $2 != *.fa && $2 != *.fasta ]]; then
-    echo "Error: The files must end with .fa or .fasta"
-    exit 1
+if [[ $ref_fasta != *.fa && $ref_fasta != *.fasta ]]; then
+  echo "Error --ref should be a file that ends in .fa or .fasta"
+  exit 1
 fi
 
-# Grab the reference and query FASTAs
-ref_fasta=$1
-query_fasta_file=$2
-
-# The length is the third argument, or 5000 if not provided
-length=${3:-5000}
+if [[ $query_fasta_file != *.fa && $query_fasta_file != *.fasta ]]; then
+  echo "Error --query should be a file that ends in .fa or .fasta"
+  exit 1
+fi
 
 # Print the filenames and length
 echo "You have provided:"
+echo -e "Found the FASTQ files: \n"${found_fastq}
 echo "Found FASTA reference file: "${ref_fasta}
 echo "Query Filename: "${query_fasta_file}
 echo "The size of the cutoff is: "${length}
 
+# Create an empty file to store the concatenated results
+cat /dev/null > concat.fastq
 
-#Extract all .gz fastq files
-gunzip *.fastq.gz
+# Loop over all arguments
+for file in ${found_fastq[@]}
+do
+    # Check if the file is a .fastq.gz file
+    if [[ $file == *.fastq.gz ]]
+    then
+        # If it is, gunzip the file and append to the output file
+        gunzip -c "$file" >> concat.fastq
+    elif [[ $file == *.fastq ]]
+    then
+        # If it's a .fastq file, just append it to the output file
+        cat "$file" >> concat.fastq
+    else
+        echo "Warning: $file is not a .fastq or .fastq.gz file and will be skipped."
+    fi
+done
 
-## Concatenate all FASTQ files together
-# Find all files generated from the GridION ("FAX"..) in the current directory with the ".fastq" extension
-files=$(find . -maxdepth 1 -name "FAX*.fastq")
-
-# Concatenate the found files
-cat $files > concat.fastq
+module load nanopack
+NanoPlot --fastq_rich concat.fastq --N50 -o ./NanoPlot
 
 # Remove the file extension from the file name
 ref_basename=$(basename "$ref_fasta")
-ref_basename="$(ref_basename%.*}"
+ref_basename="${ref_basename%.*}"
 
 echo "The base that will be used for naming is " ${ref_basename}
 echo
@@ -139,6 +216,7 @@ seqtk seq -F '#' filtered_reads.fasta > ${ref_basename}_filtered.fastq
 module load minimap2
 
 minimap2 --secondary=no --sam-hit-only -ax map-ont ${ref_fasta} ./${ref_basename}_filtered.fastq > ./${ref_basename}_mapped.sam
+minimap2 --secondary=no --sam-hit-only -ax map-ont dm6_mt_wMel.fasta ./concat.fastq > ./${ref_basename}_dm6mtwMel.sam
 
 module purge && module load samtools
 
@@ -146,6 +224,19 @@ module purge && module load samtools
 samtools sort ./${ref_basename}_mapped.sam -o ./${ref_basename}_mapped.bam
 samtools view -bq 1 ./${ref_basename}_mapped.bam > ./${ref_basename}_unique.bam
 
+samtools sort ./${ref_basename}_dm6mtwMel.sam -o ./${ref_basename}_dm6mtwMel.bam
+samtools index ./${ref_basename}_dm6mtwMel.bam
+
+samtools idxstats ./${ref_basename}_dm6mtwMel.bam | cut -f 1,3 | grep -v "\*" > output.txt
+
+echo "set terminal pdf size 8,6" > plot.gp
+echo "set output '${ref_basename}_dm6mtwMel.pdf'" >> plot.gp
+echo "set style data histograms" >> plot.gp
+echo "set style fill solid 1.0 border -1" >> plot.gp
+echo "set xtics rotate by -45" >> plot.gp
+echo "plot 'output.txt' using 2:xtic(1) title 'Number of reads aligned to each chromosome'" >> plot.gp
+gnuplot plot.gp
+rm output.txt plot.gp
 
 #Generate the consensus sequence
 module purge && module load medaka
